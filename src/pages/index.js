1a017ec8791cb38c8a30a9398e9a8bcc0b273f10
Riyaz-khan-shuvo <mdriyaz5965@gmail.com>
2023-11-01 19:55:12 +0600

    layout design has completed

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,38 +1,95 @@
-"use client"
-import { useRef, useState } from 'react'
-import data from '../../data.json'
+import { useRef, useState } from 'react';
+import data from '../../data.json';
 
 export default function Home() {
-  const [products, setProducts] = useState(data)
-  const dragProduct = useRef(0)
-  const draggedOverProduct = useRef(0)
+  const [products, setProducts] = useState(data);
+  const dragProduct = useRef(0);
+  const draggedOverProduct = useRef(0);
 
   const handleSort = () => {
-    const productsClone = [...products]
-    const temp = productsClone[dragProduct.current]
-    productsClone[dragProduct.current] = productsClone[draggedOverProduct.current]
-    productsClone[draggedOverProduct.current] = temp
-    setProducts(productsClone)
-  }
+    const productsClone = [...products];
+    const temp = productsClone[dragProduct.current];
+    productsClone[dragProduct.current] = productsClone[draggedOverProduct.current];
+    productsClone[draggedOverProduct.current] = temp;
+    setProducts(productsClone);
+  };
+
+  console.log(products);
 
   return (
-    <main className="flex min-h-screen flex-col items-center space-y-4">
+    <main className="">
       <div className="container">
-        <h1 className="text-xl font-bold mt-4">List</h1>
+        <h1 className="">List</h1>
         <div className="row">
-          {products.map((product, index) => (
+          <div className="col-md-5">
             <div
-              key={index}
-              className='col-md-4 my-3'
+              className="card"
               draggable
-              onDragStart={() => (dragProduct.current = index)}
-              onDragEnter={() => (draggedOverProduct.current = index)}
+              onDragStart={() => (dragProduct.current = 0)}
+              onDragEnter={() => (draggedOverProduct.current = 0)}
+              onDragEnd={handleSort}
+              onDragOver={(e) => e.preventDefault()}
+            >
+              <div className="p-3">
+                <img className='img-fluid' src={products[0].productImage} alt="" />
+              </div>
+            </div>
+          </div>
+          <div className="col-md-7">
+            <div className="row">
+              {products.slice(1, 7).map((product, index) => (
+                <div
+                  key={index + 1}
+                  className="col-md-4 my-3"
+                  draggable
+                  onDragStart={() => (dragProduct.current = index + 1)}
+                  onDragEnter={() => (draggedOverProduct.current = index + 1)}
+                  onDragEnd={handleSort}
+                  onDragOver={(e) => e.preventDefault()}
+                >
+                  <div className="card">
+                    <div className="p-3">
+                      <img className='img-fluid' src={product.productImage} alt="" />
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
+
+          </div>
+
+          {/* {products.slice(7).map((product, index) => (
+            <div
+              key={index + 7}
+              className="col-md-2 my-3 custom-col"
+              draggable
+              onDragStart={() => (dragProduct.current = index + 7)}
+              onDragEnter={() => (draggedOverProduct.current = index + 7)}
               onDragEnd={handleSort}
               onDragOver={(e) => e.preventDefault()}
             >
               <div className="card">
                 <div className="p-3">
-                <img className='img-fluid' src={product.productImage} alt="" />
+                  <img className='img-fluid' src={product.productImage} alt="" />
+                </div>
+              </div>
+            </div>
+          ))} */}
+        </div>
+        <div className="custom-grid">
+          {products.slice(7).map((product, index) => (
+            <div
+              key={index + 7}
+              className="custom-col" // Add a custom CSS class
+              draggable
+              onDragStart={() => (dragProduct.current = index + 7)}
+              onDragEnter={() => (draggedOverProduct.current = index + 7)}
+              onDragEnd={handleSort}
+              onDragOver={(e) => e.preventDefault()}
+            >
+              <div className="card">
+                <div className="p-3">
+                  <img className='img-fluid' src={product.productImage} alt="" />
                 </div>
               </div>
             </div>
@@ -40,5 +97,83 @@ export default function Home() {
         </div>
       </div>
     </main>
-  )
+  );
 }
+
+
+
+
+
+
+// "use client"
+// import { useRef, useState } from 'react'
+// import data from '../../data.json'
+
+// export default function Home() {
+//   const [products, setProducts] = useState(data)
+//   const dragProduct = useRef(0)
+//   const draggedOverProduct = useRef(0)
+
+//   const handleSort = () => {
+//     const productsClone = [...products]
+//     const temp = productsClone[dragProduct.current]
+//     productsClone[dragProduct.current] = productsClone[draggedOverProduct.current]
+//     productsClone[draggedOverProduct.current] = temp
+//     setProducts(productsClone)
+//   }
+
+//   return (
+//     <main className="">
+//       <div className="container">
+//         <h1 className="">List</h1>
+//         <div className="row">
+//           {products.map((product, index) => (
+//             index == 0 ? <div
+//               key={index}
+//               className='col-md-3 my-3'
+//               draggable
+//               onDragStart={() => (dragProduct.current = index)}
+//               onDragEnter={() => (draggedOverProduct.current = index)}
+//               onDragEnd={handleSort}
+//               onDragOver={(e) => e.preventDefault()}
+//             >
+//               <div className="card">
+//                 <div className="p-3">
+//                   <img className='img-fluid' src={product.productImage} alt="" />
+//                 </div>
+//               </div>
+//             </div> : index < 7 ? <div
+//               key={index}
+//               className='col-md-2 my-3'
+//               draggable
+//               onDragStart={() => (dragProduct.current = index)}
+//               onDragEnter={() => (draggedOverProduct.current = index)}
+//               onDragEnd={handleSort}
+//               onDragOver={(e) => e.preventDefault()}
+//             >
+//               <div className="card">
+//                 <div className="p-3">
+//                   <img className='img-fluid' src={product.productImage} alt="" />
+//                 </div>
+//               </div>
+//             </div> : <div
+//               key={index}
+//               className='col-md-3 my-3'
+//               draggable
+//               onDragStart={() => (dragProduct.current = index)}
+//               onDragEnter={() => (draggedOverProduct.current = index)}
+//               onDragEnd={handleSort}
+//               onDragOver={(e) => e.preventDefault()}
+//             >
+//               <div className="card">
+//                 <div className="p-3">
+//                   <img className='img-fluid' src={product.productImage} alt="" />
+//                 </div>
+//               </div>
+//             </div>
+//           ))}
+//         </div>
+//       </div>
+//     </main>
+//   )
+// }
